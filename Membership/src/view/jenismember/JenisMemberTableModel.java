1ef66019bdea88f6a263dbf809a43b21432b4ac0
Faizalhadziq <125478920+Faizalhadziq@users.noreply.github.com>
2024-11-10 20:34:19 +0700

    Add files via upload

---
+++ Membership/src/view/jenismember/JenisMemberTableModel.java
@@ -0,0 +1,52 @@
+package view.jenismember;
+
+import javax.swing.table.*;
+import java.util.List;
+import model.JenisMember;
+
+class JenisMemberTableModel extends AbstractTableModel {
+    private String[] columnNames = {"Nama"};
+    private List<JenisMember> data;
+
+    public JenisMemberTableModel(List<JenisMember> data) 
+    {
+        this.data = data;
+    }
+
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public int getRowCount() {
+        return data.size();
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col){
+        Member rowItem = data.get(row);
+        String value = "";
+        switch (col)
+        {
+            case 0:
+                value = rowItem.getNama();
+                break;
+            case 1:
+                value = rowItem.getJenisMember().getNama();
+                break;
+        }
+        return value;
+    }
+
+    public boolean isCellEditable(int row, int col) {
+        return false;
+    }
+
+    public void add(Member value)
+    {
+        data.add(value);
+        fireTableRowsInserted(data.size() - 1, data.size() -1);
+    }
+}
